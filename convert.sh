ac2c186828a0f3754287f9bddc0b7d35b856294f
Peter Grace <pete.grace@gmail.com>
2021-01-14 11:37:36 -0500

    don't need to fake a resize since the dxf is now properly sized

--- convert.sh
+++ convert.sh
@@ -4,5 +4,4 @@ rm -f *.svg
 rm -f *.png
 FILENAME=${1:-"file.dxf"}
 /usr/share/inkscape/extensions/dxf_input.py /app/${FILENAME%%.*}.dxf > /app/${FILENAME%%.*}.svg
-sed -E -i 's,dtd\" width=\".*?\" height=\".*?\">,dtd\" width="1404" height="1872">,g' /app/${FILENAME%%.*}.svg
 convert /app/${FILENAME%%.*}.{svg,png}
