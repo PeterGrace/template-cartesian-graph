652043c848240e3729e2c2c2249710383e638fc7
Peter Grace <pete.grace@gmail.com>
2021-01-14 11:53:48 -0500

    update

--- convert.sh
+++ convert.sh
@@ -2,6 +2,5 @@
 # The viewport is too small, we gotta make it bigger -- edit viewport
 rm -f *.svg
 rm -f *.png
-FILENAME=${1:-"file.dxf"}
-/usr/share/inkscape/extensions/dxf_input.py /app/${FILENAME%%.*}.dxf > /app/${FILENAME%%.*}.svg
+FILENAME=${1:-"file.svg"}
 convert /app/${FILENAME%%.*}.{svg,png}
